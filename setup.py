9ef23000cdb0492ad946a71860bdfbdd30d41e4d
zariiii9003 <52598363+zariiii9003@users.noreply.github.com>
2022-08-13 16:34:23 +0200

    add PYPI classifiers and description

--- setup.py
+++ setup.py
@@ -19,6 +19,9 @@ with open(pkg_dir / "__init__.py") as f:
             version = line.split("=")[-1].strip().strip('"')
             break
 
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
 
 if not os.getenv("VXLAPI_DIR"):
     raise RuntimeError("Environment variable VXLAPI_DIR not set.")
@@ -51,6 +54,21 @@ setup(
     version=version,
     url="https://github.com/zariiii9003/python-can-cvector",
     license="LGPL v3",
+    description="A partly cythonized version of the python-can VectorBus",
+    long_description=long_description,
+    classifiers=[
+        # a list of all available ones: https://pypi.org/classifiers/
+        "Programming Language :: Cython",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Operating System :: Microsoft :: Windows",
+        "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
+        "Intended Audience :: Developers",
+    ],
     packages=find_packages("src"),
     package_dir={"": "src"},
     package_data=package_data,
